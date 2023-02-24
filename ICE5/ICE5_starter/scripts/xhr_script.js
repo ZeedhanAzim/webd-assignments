7d08f6c25b7157eff99c9eab013bb9bfa93691fe
Zeedhan Azim <zeedhanazim@gmail.com>
2023-02-24 13:38:11 -0500

    completed ICE 5, addd ICE 6

--- ICE5/ICE5_starter/scripts/xhr_script.js
+++ ICE5/ICE5_starter/scripts/xhr_script.js
@@ -8,22 +8,48 @@ let url_todos = 'https://jsonplaceholder.typicode.com/todos';
 let url_users = 'https://jsonplaceholder.typicode.com/users';
 
 // instantiate the XMLHttpRequest object with the new keyword
+const xhr = new XMLHttpRequest();
 
 
 
 // create a callback function to fire when the onreadystatechange happens
+xhr.onreadystatechange = () => {
+    console.log(`the state: ${xhr.readyState}`);
     // check that the state is done
-    // if (xhr.readyState === 4) {
+    // if (xhr.readyState === XMLHttpRequest.DONE)
+    if (xhr.readyState === 4) {
+
+        if (xhr.readyState === 200) {
+
             // turn into json
+            let jsonRes = JSON.parse(xhr.responseText);
+
             // console log to see what we have
+            console.log(`the response: ${jsonRes[0]['url']}`);
+
             // update the 1st image
+            $("#photo1").attr("src", jsonRes[0]['url']);
             // update the 1st figcaption
+            $("#photo1 ~ figCaption").text("first image returned from ajax call")
             // update the 2nd image
+            $("#photo2").attr("src", jsonRes[1]['url']);
             // update the 2nd figcaption
+            $("#photo2 ~ figCaption").text("second image returned from ajax call")
             // send error message
+        }
+        else {
+            alert(`error with call: ${xhr.status}`)
+        }
+
+    }
+
+}
+
 
 // use the .open() method to configure the object
-
+xhr.open("GET", url_photos);
 // add datatype to header
+//xhr.setRequestHeader{"Content-type", "application/json"};
 
 // use the .send() method to send the request
+xhr.send();
