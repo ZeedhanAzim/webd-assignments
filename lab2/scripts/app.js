338e0d3464915b56d33210f74babe50ca3c83c29
Zeedhan Azim <zeedhanazim@gmail.com>
2023-03-02 21:58:55 -0500

    fleshed out validation more

--- lab2/scripts/app.js
+++ lab2/scripts/app.js
@@ -5,8 +5,16 @@ Date Completed: 2023-
 */
 
 
-
-console.log("js success")
+//create User class for when user registers
+class User {
+    constructor(fName, lName, uName, email, passw) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.userName = uName;
+        this.email = email;
+        this.password = passw;
+    }
+}
 
 
 //when contact email is submitted, this function will bring user back to index 
@@ -82,22 +90,82 @@ function createBottomNav() {
 
 //register validation
 function validateRegister() {
+
+    //constants for validation
+    const MIN_NAME_LENGTH = 2;
+
+    //values for potential errors
+    let errorMessage = "";
+
     // get values
-    let firstName = $("#firstNameInput").val();
-    let lastName = $("#lastNameInput").val();
-    let email = $("#emailInput").val();
-    let password = $("#passwordInput").val();
-    let confirmPassword = $("#confirmPasswordInput").val();
-    let buttonPress = $("#btnSubmit").val;
+    let firstName = $("#firstNameInput").val().trim();
+    let lastName = $("#lastNameInput").val().trim();
+    let email = $("#emailInput").val().trim();
+    let password = $("#passwordInput").val().trim();
+    let confirmPassword = $("#confirmPasswordInput").val().trim();
+    let buttonPress = $("#btnSubmit").val.trim();
+
+    //variables that will be used when making user object
+    let fNameVar = "";
+    let lNameVar = "";
+    let emailVar = "";
+    let passwVar = "";
+    let confirmPasswVar = "";
 
     //check if fields are all filled
     if (!firstName || !lastName || !email || !password || !confirmPassword) {
         alert("please fill out all fields")
         return false;
     }
+    else {
+
+        //first name validation
+        if (firstName.length < MIN_NAME_LENGTH) {
+            errorMessage = "first name must be at least " + MIN_NAME_LENGTH
+                + " characters";
+            //output error message
+            const errorPara = document.createElement("p");
+            errorPara.innerHTML = errorMessage
+        }
+        //last name validation
+        if (lastName.length < MIN_NAME_LENGTH) {
+            errorMessage = "last name must be at least " + MIN_NAME_LENGTH
+                + " characters";
+        }
+
+        //confirm password validation
+        if (confirmPassword != password) { }
+
+        //if everything is valid
+        if (fNameVar.length != MIN_NAME_LENGTH && lNameVar != MIN_NAME_LENGTH
+            && emailVar != 0 && passwVar != 0 && passwVar === confirmPasswVar) {
+
+            //create user object and display in console
+            const newUser = new User(fNameVar, lNameVar, emailVar, passwordVar)
+            console.log(`User: ${newUser.firstName} ${newUser.lastName}\nEmail: ${newUser.email}\nPassword: ${newUser.password}`);
+
+        }
+    }
+
+
 
 }
 
-//perform function when button is clicked
-$('#btnSubmit').click(function (event) {
+
+//validation for login form
+function validateLogin() {
+
+    let userName = $("#usernameInput").val().trim();
+    let password = $("#passwordInput").val().trim();
+
+    if (!userName || !password) {
+        alert("please fill out all fields")
+        return false;
+    }
+    else {
+
+
+        //notify user of success
+
+    }
 }
