30c475d6ee5a2bd0be68cd8f119988fecc20b179
Zeedhan Azim <zeedhanazim@gmail.com>
2023-03-03 21:34:04 -0500

    finished form validation

--- lab2/scripts/app.js
+++ lab2/scripts/app.js
@@ -16,7 +16,6 @@ class User {
     }
 }
 
-
 //when contact email is submitted, this function will bring user back to index 
 function timedRedirect(e) {
     e.preventDefault();
@@ -40,7 +39,6 @@ let submitButton = document.getElementsByClassName('btn-primary')[0];
 if (submitButton) {
     submitButton.addEventListener('click', timedRedirect, false);
 }
-
 //text for product page
 function serviceText() {
     document.write("<p>My key skills and traits include:<li>");
@@ -50,13 +48,10 @@ function serviceText() {
     document.write("<ul>Ability to work independently and in a team</ul>");
     document.write("</li></p>");
 }
-
 //text for About me page
 function productText() {
 
 }
-
-
 //text for About me page
 function aboutText() {
     document.write(
@@ -88,16 +83,29 @@ function createBottomNav() {
     body.appendChild(bottomNav);
 }
 
+//function to create div
+function newDiv() {
+    const divider = document.createElement('div');
+    divider.setAttribute('id', 'ErrorMessage');
+}
+
+
 //register validation
-function validateRegister() {
+function validateRegister(e) {
 
-    //constants for validation
+    e.preventDefault();
+
+    //constants used for validating user input
     const MIN_NAME_LENGTH = 2;
+    //regex found from: https://masteringjs.io/tutorials/fundamentals/email-regex
+    const EMAIL_REGEX = "/[^\s@]+@[^\s@]+\.[^\s@]+/";
+    const MIN_EMAIL_LENGTH = 8;
+    const MIN_PASSWORD_LENGTH = 6;
 
-    //values for potential errors
+    //value for potential error messages
     let errorMessage = "";
 
-    // get values
+    //values to get user input
     let firstName = $("#firstNameInput").val().trim();
     let lastName = $("#lastNameInput").val().trim();
     let email = $("#emailInput").val().trim();
@@ -115,57 +123,89 @@ function validateRegister() {
     //check if fields are all filled
     if (!firstName || !lastName || !email || !password || !confirmPassword) {
         alert("please fill out all fields")
-        return false;
     }
     else {
 
         //first name validation
         if (firstName.length < MIN_NAME_LENGTH) {
-            errorMessage = "first name must be at least " + MIN_NAME_LENGTH
+
+            errorMessage = "First name must be at least " + MIN_NAME_LENGTH
                 + " characters";
             //output error message
-            const errorPara = document.createElement("p");
-            errorPara.innerHTML = errorMessage
+            $("#ErrorMessage")[0].append(errorMessage);
         }
         //last name validation
         if (lastName.length < MIN_NAME_LENGTH) {
-            errorMessage = "last name must be at least " + MIN_NAME_LENGTH
+            errorMessage = "Last name must be at least " + MIN_NAME_LENGTH
                 + " characters";
+            $("#ErrorMessage")[1].append(errorMessage);
+        }
+
+        //email validation
+        //regex check
+        if (!email.match(EMAIL_REGEX)) {
+            errorMessage = "Email address entered was invalid";
+            $("#ErrorMessage")[3].append(errorMessage);
+        }
+
+        //email length check
+        if (email.length < MIN_EMAIL_LENGTH) {
+            errorMessage = "Email address must be 8 characters length";
+            $("#ErrorMessage")[3].append(errorMessage);
+        }
+
+        //password check
+        if (password.length != MIN_PASSWORD_LENGTH) {
+            errorMessage = "Password must be at least " + MIN_PASSWORD_LENGTH + " characters in length";
+            $("#ErrorMessage")[4].append(errorMessage);
         }
 
         //confirm password validation
-        if (confirmPassword != password) { }
+        if (confirmPassword != password) {
+            errorMessage = "Password must be at least " + MIN_PASSWORD_LENGTH + " characters in length";
+            $("#ErrorMessage")[5].append(errorMessage);
+        }
 
         //if everything is valid
-        if (fNameVar.length != MIN_NAME_LENGTH && lNameVar != MIN_NAME_LENGTH
-            && emailVar != 0 && passwVar != 0 && passwVar === confirmPasswVar) {
+        if (fNameVar.length != 0 && lNameVar != 0 && emailVar != 0 &&
+            passwVar != 0 && passwVar === confirmPasswVar) {
 
             //create user object and display in console
             const newUser = new User(fNameVar, lNameVar, emailVar, passwordVar)
             console.log(`User: ${newUser.firstName} ${newUser.lastName}\nEmail: ${newUser.email}\nPassword: ${newUser.password}`);
 
+            //clear all error messages    
+            $("#ErrorMessage")[0].append("");
+            $("#ErrorMessage")[1].append("");
+            $("#ErrorMessage")[2].append("");
+            $("#ErrorMessage")[3].append("");
+            $("#ErrorMessage")[4].append("");
+            $("#ErrorMessage")[5].append("");
         }
     }
-
-
-
 }
 
-
 //validation for login form
-function validateLogin() {
+function validateLogin(e) {
 
+    e.preventDefault();
+
+    //constants for validation
+    const MIN_NAME_LENGTH = 2;
+
+    //values to get user input
     let userName = $("#usernameInput").val().trim();
     let password = $("#passwordInput").val().trim();
 
     if (!userName || !password) {
-        alert("please fill out all fields")
+        const errorMessage = document.createTextNode("please fill out all fields");
+        $("#ErrorMessage")[0].append(errorMessage);
         return false;
     }
     else {
 
-
         //notify user of success
-
+        const userNameText = document.createTextNode(newUser.userName);
+        $("#userNameDisplay").append(userNameText);
     }
 }
