2542a6c874c77ed25dd22a60af214abbe21464c2
Zeedhan Azim <zeedhanazim@gmail.com>
2023-03-17 14:37:10 -0400

    added ICE 8

---
+++ ICE8_05/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const PORT = 3000;
+
+//create express app
+const app = express();
+
+//set up the Express-Handlebars
+app.engine('handlebars', exphbs.engine({ defaultlayout: 'main' }))
+app.set('view engine', 'handlebars');
+app.set('view', './views')
+
+app.use((req, res, next) => {
+    console.log(`URL: ${req.url}`);
+    req.myName = 'Zeedhan';
+    next()
+})
+
+//additional middleware can be added here, like authentication, error handling, authorization
+
+//route to '/' with get method
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get("/", (req, res) => {
+    res.send('this is the about page for the Express app ');
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
