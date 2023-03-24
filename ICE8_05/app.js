c3efb95930fd6ed15c6da797b8e9e8286e745d78
Zeedhan Azim <zeedhanazim@gmail.com>
2023-03-23 20:33:44 -0400

    added ICE 9

--- ICE8_05/app.js
+++ ICE8_05/app.js
@@ -4,9 +4,9 @@ const PORT = 3000;
 //create express app
 const app = express();
 
-//set up the Express-Handlebars
-app.engine('handlebars', exphbs.engine({ defaultlayout: 'main' }))
-app.set('view engine', 'handlebars');
+//set up the Express-Handlebars engine
+app.engine('hbs', exphbs.engine({ defaultlayout: 'main', extname: '.hbs', partialsDir: './views/partials' }))
+app.set('view engine', 'hbs');
 app.set('view', './views')
 
 //engage in middleware
@@ -20,20 +20,29 @@ app.use((req, res, next) => {
 
 //route to '/' with get method
 app.get("/", (req, res) => {
-    res.render('home');
+    const homeh1 = "welcome";
+    const pageTitle = "home page";
+    res.render('home', {
+        pageTitle: pageTitle,
+        homeh1: homeh1
+    });
 });
 
 app.get("/", (req, res) => {
+    const pageTitle = "home page";
     res.send('this is the about page for the Express app ');
 });
 
 //get second handlebar
 app.get("/", (req, res) => {
+    const pageTitle = "new home page";
     res.render('new_home');
 });
 
-app.get("/", (req, res) => {
-    res.send('this is the new about page for the Express app ');
+app.get("/new_home", (req, res) => {
+    res.render('this is the new about page for the Express app ', {
+        pageTitle: pageTitle
+    });
 });
 
 
---
+++ ICE9/controllers/userController.js
@@ -0,0 +1,13 @@
+//for home get
+const homeView = (req, res) => {
+    const pageTitle = "HOME PAGE";
+    const homeContent = "welcome to the homepage";
+    res.render(home, {
+        pageTitle: pageTitle,
+        homeContent: homeContent
+    })
+};
+
+module.exports = {
+    homeView
+}
